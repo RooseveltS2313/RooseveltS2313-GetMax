f0a33a114d1fb55a2519466db00ef4bf12239e0d
Roosevelt Seymour <RSeymourJR04@outlook.com>
2023-02-13 04:24:19 +0000

    Completed GetMax Challenge R Seymour Jr

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,15 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        // doing this to always have a value incase array only has values less than 0 unless array is empty but then it hits OoB exception and can be handled in future
+        int largest = arr[0];
+        // which means this can start at 1 or be ++i instead of i++ but does not have to
+        for (int i = 0; i < arr.length; ++i){
+            // no reason to switch if equal to
+            if( largest < arr[i]){
+                largest = arr[i];
+            }
+        }
+        return largest;
     }
 }
